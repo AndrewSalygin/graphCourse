7344b09b7c30eb27a71b6e4364ddf46b2af1dad3
Andrew Salygin <andrew.salygin@gmail.com>
2023-09-13 16:33:18 +0400

    Test deleteNode and deleteArc methods

--- src/test/java/OrientedUnweightedGraphTest.java
+++ src/test/java/OrientedUnweightedGraphTest.java
@@ -1,15 +1,15 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ru.andrewsalygin.graph.Graph;
 import ru.andrewsalygin.graph.Node;
 import ru.andrewsalygin.graph.OrientedUnweightedGraph;
 import ru.andrewsalygin.graph.OrientedWeightedGraph;
+import ru.andrewsalygin.graph.utils.ConnectionNotExistException;
 import ru.andrewsalygin.graph.utils.NodeAlreadyExistException;
 import ru.andrewsalygin.graph.utils.NodeNotExistException;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.HashMap;
 
 /**
  * @author Andrew Salygin
@@ -22,6 +22,8 @@ class OrientedUnweightedGraphTest {
     void setUp() {
         graph = new OrientedWeightedGraph<>();
     }
+
+    // Тесты для нод
     @Test
     void addNode_withoutExceptions() {
         Assertions.assertDoesNotThrow(() -> graph.addNode(5));
@@ -57,6 +59,63 @@ class OrientedUnweightedGraphTest {
     }
 
     @Test
+    void deleteNode_nodeNotExist() {
+        Assertions.assertThrows(NodeNotExistException.class, () -> graph.deleteNode(5));
+    }
+    @Test
+    void deleteNode_nodeExistWithoutOthers() {
+        graph.addNode(5);
+        Assertions.assertDoesNotThrow(() -> graph.deleteNode(5));
+    }
+
+    @Test
+    void deleteNode_nodeExistWithOneNodeToDeleteNode() {
+        graph.addNode(5);
+        graph.addNode(7);
+        graph.addArc(7, 5);
+        Assertions.assertDoesNotThrow(() -> graph.deleteNode(5));
+        Assertions.assertEquals(new HashMap<>(), graph.getConnectedNodes(7));
+    }
+
+    @Test
+    void deleteNode_nodeExistWithSeveralNodesToDeleteNode() {
+        graph.addNode(5);
+        graph.addNode(7);
+        graph.addNode(10);
+        graph.addArc(7, 5);
+        graph.addArc(10, 5);
+        Assertions.assertDoesNotThrow(() -> graph.deleteNode(5));
+        Assertions.assertEquals(new HashMap<>(), graph.getConnectedNodes(7));
+        Assertions.assertEquals(new HashMap<>(), graph.getConnectedNodes(10));
+    }
+
+    @Test
+    void deleteNode_nodeExistWithOneNodeToDeleteAndSeveralNodes() {
+        graph.addNode(5);
+        graph.addNode(7);
+        graph.addNode(10);
+        graph.addArc(7, 5);
+        graph.addArc(10, 5);
+        Assertions.assertDoesNotThrow(() -> graph.deleteNode(5));
+        Assertions.assertEquals(new HashMap<>(), graph.getConnectedNodes(7));
+        Assertions.assertEquals(new HashMap<>(), graph.getConnectedNodes(10));
+    }
+
+    @Test
+    void deleteNode_deleteTransitiveNode() {
+        graph.addNode(5);
+        graph.addNode(7);
+        graph.addNode(10);
+        graph.addArc(5, 7);
+        graph.addArc(7, 10);
+        graph.addArc(10, 7);
+        Assertions.assertDoesNotThrow(() -> graph.deleteNode(7));
+        Assertions.assertEquals(new HashMap<>(), graph.getConnectedNodes(5));
+        Assertions.assertEquals(new HashMap<>(), graph.getConnectedNodes(10));
+    }
+
+    // Тесты для дуг
+    @Test
     void addArc_withEmptyHMAndTwoNodes() {
         graph.addNode(5);
         graph.addNode(7);
@@ -73,5 +132,38 @@ class OrientedUnweightedGraphTest {
     void addArc_withEmptyHMAndOneNodeDest() {
         graph.addNode(5);
         Assertions.assertThrows(NodeNotExistException.class, () -> graph.addArc(7, 5));
+    }
+
+    @Test
+    void getConnectedNodes_test() {
+        graph.addNode(5);
+        graph.addNode(7);
+        graph.addArc(5, 7);
+        HashMap<Node<Integer>, Integer> connectedNodes = new HashMap<>();
+        connectedNodes.put(new Node<>(7), 0);
+        Assertions.assertEquals(connectedNodes, graph.getConnectedNodes(5));
+    }
+
+    @Test
+    void deleteArc_notExistArc() {
+        graph.addNode(5);
+        graph.addNode(7);
+        Assertions.assertThrows(ConnectionNotExistException.class, () -> graph.deleteArc(5, 7));
+    }
+
+    @Test
+    void deleteArc_betweenTwoNodes() {
+        graph.addNode(5);
+        graph.addNode(7);
+        graph.addArc(5, 7);
+        Assertions.assertDoesNotThrow(() -> graph.deleteArc(5, 7));
+    }
+
+    @Test
+    void deleteArc_wrongDirectionTest() {
+        graph.addNode(5);
+        graph.addNode(7);
+        graph.addArc(5, 7);
+        Assertions.assertThrows(ConnectionNotExistException.class, () -> graph.deleteArc(7, 5));
     }
 }
