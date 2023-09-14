0b1313d32b82b93a83bcf1a474745c8a151b354c
Andrew Salygin <andrew.salygin@gmail.com>
2023-09-14 15:50:22 +0400

    Add example graph in main class

--- src/main/java/ru/andrewsalygin/Main.java
+++ src/main/java/ru/andrewsalygin/Main.java
@@ -2,38 +2,22 @@ package ru.andrewsalygin;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import ru.andrewsalygin.graph.Node;
 import ru.andrewsalygin.graph.OrientedUnweightedGraph;
-
-import java.io.*;
-import java.util.HashMap;
+import ru.andrewsalygin.graph.OrientedWeightedGraph;
 
 /**
  * @author Andrew Salygin
  */
 public class Main {
     public static void main(String[] args) throws JsonProcessingException {
-        OrientedUnweightedGraph<Integer> graph = new OrientedUnweightedGraph<>();
-        graph.addNode(1);
-        graph.addNode(2);
-        // Сериализация
-        try (FileOutputStream fileOut = new FileOutputStream("complexMap.ser");
-             ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
-            out.writeObject(graph);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // Десериализация
-        try (FileInputStream fileIn = new FileInputStream("complexMap.ser");
-             ObjectInputStream in = new ObjectInputStream(fileIn)) {
-            OrientedUnweightedGraph<Integer> deserializedGraph = (OrientedUnweightedGraph<Integer>) in.readObject();
-            deserializedGraph.deleteNode(1);
-            deserializedGraph.deleteNode(2);
-            // Используйте deserializedMap как нужно
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        ObjectMapper mapper = new ObjectMapper();
+        OrientedWeightedGraph graph = new OrientedWeightedGraph();
+        graph.addNode("5");
+        graph.addNode("7");
+        graph.addConnection("5", "7");
+        String mpJson = mapper.writeValueAsString(graph);
+        OrientedUnweightedGraph mpJsonValue = mapper.readValue(mpJson, OrientedUnweightedGraph.class);
+        System.out.println(mpJson);
     }
 }
 
@@ -41,25 +25,28 @@ public class Main {
 /*
 ObjectMapper mapper = new ObjectMapper();
         mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        SimpleModule module = new SimpleModule();
+        module.addKeyDeserializer(Node.class, new NodeDeserializer());
+        mapper.registerModule(module);
 
         OrientedUnweightedGraph<Integer> graph = new OrientedUnweightedGraph<>();
-        graph.addNode(5);
-        graph.addNode(7);
+        graph.addNode("5");
+        graph.addNode("7");
 
-        graph.addConnection(5, 7);
+        graph.addConnection("5", "7");
         String graphJson = mapper.writeValueAsString(graph);
         System.out.println(graphJson);
-        TypeReference<HashMap<Node<Integer>, HashMap<Node<Integer>, Integer>>> typeReference =
-                new TypeReference<HashMap<Node<Integer>, HashMap<Node<Integer>, Integer>>>() {};
+        TypeReference<HashMap<Node, HashMap<Node, Integer>>> typeReference =
+                new TypeReference<HashMap<Node, HashMap<Node, Integer>>>() {};
 
         // Deserialize the JSON into your map
-        HashMap<Node<Integer>, HashMap<Node<Integer>, Integer>> map = mapper.readValue(graphJson, typeReference);
+        HashMap<Node, HashMap<Node, Integer>> map = mapper.readValue(graphJson, typeReference);
 
         // Use the deserialized map
-        for (Node<Integer> outerKey : map.keySet()) {
+        for (Node outerKey : map.keySet()) {
             System.out.println("Outer Key: " + outerKey);
-            HashMap<Node<Integer>, Integer> innerMap = map.get(outerKey);
-            for (Node<Integer> innerKey : innerMap.keySet()) {
+            HashMap<Node, Integer> innerMap = map.get(outerKey);
+            for (Node innerKey : innerMap.keySet()) {
                 System.out.println("Inner Key: " + innerKey + ", Value: " + innerMap.get(innerKey));
             }
         }
