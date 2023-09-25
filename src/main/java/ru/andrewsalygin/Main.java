ccd7c74dcd7bd5d6e1679e63b0ee73ff447f1fb2
Andrew Salygin <andrew.salygin@gmail.com>
2023-09-25 20:23:50 +0400

    Create window for game and project set up for a game

--- src/main/java/ru/andrewsalygin/Main.java
+++ src/main/java/ru/andrewsalygin/Main.java
@@ -1,11 +1,23 @@
 package ru.andrewsalygin;
-import ru.andrewsalygin.graph.console.Console;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+import ru.andrewsalygin.graph.game.Game;
+
+import java.io.File;
 
 /**
  * @author Andrew Salygin
  */
 public class Main {
     public static void main(String[] args) {
-        Console.launchApplication();
+        System.setProperty("org.lwjgl.librarypath", String.valueOf(new File(System.getProperty("user.dir"), "/libs/slick2d")));
+
+        try {
+            AppGameContainer app = new AppGameContainer(new Game());
+            app.setDisplayMode(800, 600, false);
+            app.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
     }
 }
